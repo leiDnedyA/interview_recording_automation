1b157ea52ea5f53a0fec1356bda292b22e6b8546
leiDnedyA <aydendiel@gmail.com>
2024-01-31 10:56:00 -0500

    fix: fixed issue with tilde path expansion

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ destination_directory = os.getenv("DESTINATION_DIRECTORY")
 target_file_substring = os.getenv("TARGET_FILE_SUBSTRING") # substring of file in target subdirectory
 remotasks_email = os.getenv("REMOTASKS_EMAIL")
 
+source_directory = os.path.expanduser(source_directory)
+destination_directory = os.path.expanduser(destination_directory)
+
 def open_file_explorer(directory):
     try:
         subprocess.Popen(["open", directory])
@@ -24,8 +27,8 @@ def get_formatted_date():
     return formatted_date
 
 def search_and_copy_file(source_dir, destination_dir, file_substring, user_email):
+    """ Searches for and copies recording file. Returns 0 if successful and 1 otherwise. """
     for filename in os.listdir(source_dir):
-
         if file_substring.upper() in filename.upper():
             subdirectory_path = os.path.join(source_dir, filename)
             target_file_path = None
@@ -33,10 +36,10 @@ def search_and_copy_file(source_dir, destination_dir, file_substring, user_email
             try:
                 # Iterate through subdirectory
                 for filename in os.listdir(subdirectory_path):
-                    print(filename)
                     if target_file_substring in filename:
                         target_file_path = os.path.join(subdirectory_path, filename)
 
+                print(target_file_path)
                 # Check to see if target file exists in target directory
                 if (not target_file_path):
                     raise Exception(f"No file with substring '{target_file_substring}' in target directory '{subdirectory_path}'")
@@ -47,12 +50,15 @@ def search_and_copy_file(source_dir, destination_dir, file_substring, user_email
                 # Copy the file to the destination directory
                 shutil.copy(target_file_path, output_path)
                 print(f"File '{target_file_path}' copied successfully as '{output_filename}'.")
+                return 0
             except FileNotFoundError:
                 print(f"Error: File with substring '{target_file_substring}' not found in directory {subdirectory_path}.")
             except PermissionError:
                 print(f"Error: Permission denied for file '{target_file_path}'.")
             except Exception as e:
                 print(f"Error: {e}")
+    return 1
+
 def main():
     parser = argparse.ArgumentParser(description="Search and copy files based on a substring in the file name.")
     parser.add_argument("directory_keyword", help="Substring to search for target directory.")
@@ -60,7 +66,11 @@ def main():
 
     args = parser.parse_args()
 
-    search_and_copy_file(source_directory, destination_directory, args.directory_keyword, args.user_email)
+    result = search_and_copy_file(source_directory, destination_directory, args.directory_keyword, args.user_email)
+
+    if result != 0:
+        print('Error: there was a problem.')
+        return
 
     open_file_explorer(destination_directory)
 
